5ed608653406bc521dd4af9028ae7b1d6501cb8b
Lendoy <angelonovero69@gmail.com>
2026-09-05 21:14:27 +0800

    configuration of port

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5174,
+    port: 5173,
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
